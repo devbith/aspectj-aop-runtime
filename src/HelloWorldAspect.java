8f742acb1dba47f5c7246458592b61973fc021dc
Bishal Thapa <bishal.thapa@trustly.com>
2023-04-22 00:15:43 +0100

    first commit

---
+++ src/HelloWorldAspect.java
@@ -0,0 +1,12 @@
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.After;
+
+@Aspect
+public class HelloWorldAspect {
+
+  @After("execution (* HelloWorld.*(..))")
+  public void greetAspect() {
+      System.out.println(", World");
+  }
+}
---
+++ src/Runner.java
@@ -0,0 +1,9 @@
+public class Runner {
+
+  public static void main(String[] args) {
+
+      HelloWorld h = new HelloWorld();
+      h.greet();
+  }
+
+}
